c2b53ff748c1d7ff09fbf33ff7100a58cf967e74
Nanjaa <steph.o-91@hotmail.com>
2015-10-05 16:48:58 -0500

    Added a button to change the time of day manually, and began working on that button's functions

--- script.js
+++ script.js
@@ -22,9 +22,33 @@ $(document).ready(function() {
 			marginTop: '0'				
 		}, 2000, 'linear', function() {
 			$('.nightSky').fadeIn(1000);
+			$('.icon-sun-3').hide();
+			$('.icon-moon-2').show();
+			console.log(stop)
 			var wait2 = setTimeout(function() {
+				console.log('rip');
 				sunrise();
-			}, 5000);
+			}, 5000);				
+			$('.icon-moon-2').on('click', function() {
+				clearTimeout(wait2);
+				quickRise();
+			});
+		});
+	}
+	function quickSet() {
+		$('.sunset').fadeIn(125);
+		$('.darknessWrap').fadeIn(500);
+		$('.flameLight').fadeIn(500);
+		$('.sunset').animate({
+			marginTop: '0'				
+		}, 500, 'linear', function() {
+			$('.nightSky').fadeIn(125);
+			$('.icon-sun-3').hide();
+			$('.icon-moon-2').show();
+			
+			$('.icon-moon-2').on('click', function() {
+				quickRise();
+			});
 		});
 	}
 	function sunrise() {
@@ -35,12 +59,38 @@ $(document).ready(function() {
 			marginTop: '-260px'
 		}, 2000, 'linear', function() {
 			$('.sunset').fadeOut(1000);
+			$('.icon-moon-2').hide();
+			$('.icon-sun-3').show();
 			var wait3 = setTimeout(function() {
+				console.log('rip');
 				sunset();
-			}, 5000);
+			}, 5000);				
+			$('.icon-sun-3').on('click', function() {
+				clearTimeout(wait3);
+				quickSet();
+			});
+		});
+	}
+	function quickRise() {
+		$('.nightSky').fadeOut(250);
+		$('.darknessWrap').fadeOut(500);
+		$('.flameLight').fadeOut(500);
+		$('.sunset').animate({
+			marginTop: '-260px'
+		}, 500, 'linear', function() {
+			$('.sunset').fadeOut(125);
+			$('.icon-moon-2').hide();
+			$('.icon-sun-3').show();				
+			$('.icon-sun-3').on('click', function() {
+				quickSet();
+			});
 		});
 	}
 	var wait = setTimeout(function() {
 		sunset();
 	}, 5000);
+	$('.icon-sun-3').on('click', function() {
+		clearTimeout(wait);
+		quickSet();
+	})
 });
