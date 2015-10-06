1f8a8d5dc68f937d2246895bb656e3fb53a7cc32
Nanjaa <steph.o-91@hotmail.com>
2015-10-06 10:17:32 -0500

    Added a variable to fix the transition if you click the button

--- script.js
+++ script.js
@@ -13,82 +13,102 @@ $(document).ready(function() {
 	// $('a').mouseleave(function() {
 	// 	$(this).css('color', 'pink');
 	// });
+	function waitToGo(timeout) {
+		var wait4 = setTimeout(function() {
+			changeGo = true;
+		}, timeout)
+	}
+
+	var changeGo = true;
 
 	function sunset() {
-		$('.sunset').fadeIn(1000);
-		$('.darknessWrap').fadeIn(4000);
-		$('.flameLight').fadeIn(4000);
-		$('.stars').fadeIn(4000);
-		$('.sunset').animate({
-			marginTop: '0'				
-		}, 2000, 'linear', function() {
-			$('.nightSky').fadeIn(1000);
-			$('.icon-sun-3').hide();
-			$('.icon-moon-2').show();
-			console.log(stop)
-			var wait2 = setTimeout(function() {
-				console.log('rip');
-				sunrise();
-			}, 5000);				
-			$('.icon-moon-2').on('click', function() {
-				clearTimeout(wait2);
-				quickRise();
-			});
-		});
+		if(changeGo) {
+			changeGo = false;
+			$('.sunset').fadeIn(1000);
+			$('.darknessWrap').fadeIn(4000);
+			$('.flameLight').fadeIn(4000);
+			$('.stars').fadeIn(4000);
+			$('.sunset').animate({
+				marginTop: '0'				
+			}, 2000, 'linear', function() {
+				$('.nightSky').fadeIn(1000);
+				waitToGo(1000);
+				$('.icon-sun-3').hide();
+				$('.icon-moon-2').show();
+				var wait2 = setTimeout(function() {
+					sunrise();
+				}, 10000);				
+				$('.icon-moon-2').on('click', function() {
+					clearTimeout(wait2);
+					quickRise();
+				});
+			});			
+		}
 	}
 	function quickSet() {
-		$('.sunset').fadeIn(125);
-		$('.darknessWrap').fadeIn(500);
-		$('.flameLight').fadeIn(500);
-		$('.stars').fadeIn(500);
-		$('.sunset').animate({
-			marginTop: '0'				
-		}, 500, 'linear', function() {
-			$('.nightSky').fadeIn(125);
-			$('.icon-sun-3').hide();
-			$('.icon-moon-2').show();
-			
-			$('.icon-moon-2').on('click', function() {
-				quickRise();
-			});
-		});
+		if(changeGo) {
+			changeGo = false;
+			$('.sunset').fadeIn(125);
+			$('.darknessWrap').fadeIn(500);
+			$('.flameLight').fadeIn(500);
+			$('.stars').fadeIn(500);
+			$('.sunset').animate({
+				marginTop: '0'				
+			}, 500, 'linear', function() {
+				$('.nightSky').fadeIn(125);
+				waitToGo(125);
+				$('.icon-sun-3').hide();
+				$('.icon-moon-2').show();
+				
+				$('.icon-moon-2').on('click', function() {
+					quickRise();
+				});
+			});			
+		}
 	}
 	function sunrise() {
-		$('.nightSky').fadeOut(1000);
-		$('.darknessWrap').fadeOut(4000);
-		$('.flameLight').fadeOut(4000);
-		$('.stars').fadeOut(4000);
-		$('.sunset').animate({
-			marginTop: '-260px'
-		}, 2000, 'linear', function() {
-			$('.sunset').fadeOut(1000);
-			$('.icon-moon-2').hide();
-			$('.icon-sun-3').show();
-			var wait3 = setTimeout(function() {
-				console.log('rip');
-				sunset();
-			}, 5000);				
-			$('.icon-sun-3').on('click', function() {
-				clearTimeout(wait3);
-				quickSet();
-			});
-		});
+		if(changeGo) {
+			changeGo = false;
+			$('.nightSky').fadeOut(1000);
+			$('.darknessWrap').fadeOut(4000);
+			$('.flameLight').fadeOut(4000);
+			$('.stars').fadeOut(4000);
+			$('.sunset').animate({
+				marginTop: '-260px'
+			}, 2000, 'linear', function() {
+				$('.sunset').fadeOut(1000);
+				waitToGo(1000);
+				$('.icon-moon-2').hide();
+				$('.icon-sun-3').show();
+				var wait3 = setTimeout(function() {
+					sunset();
+				}, 10000);				
+				$('.icon-sun-3').on('click', function() {
+					clearTimeout(wait3);
+					quickSet();
+				});
+			});			
+		}
 	}
 	function quickRise() {
-		$('.nightSky').fadeOut(250);
-		$('.darknessWrap').fadeOut(500);
-		$('.flameLight').fadeOut(500);
-		$('.stars').fadeOut(500);
-		$('.sunset').animate({
-			marginTop: '-260px'
-		}, 500, 'linear', function() {
-			$('.sunset').fadeOut(125);
-			$('.icon-moon-2').hide();
-			$('.icon-sun-3').show();				
-			$('.icon-sun-3').on('click', function() {
-				quickSet();
-			});
-		});
+		if(changeGo) {
+			changeGo = false;
+			$('.nightSky').fadeOut(250);
+			$('.darknessWrap').fadeOut(500);
+			$('.flameLight').fadeOut(500);
+			$('.stars').fadeOut(500);
+			$('.sunset').animate({
+				marginTop: '-260px'
+			}, 500, 'linear', function() {
+				$('.sunset').fadeOut(125);
+				waitToGo(125);
+				$('.icon-moon-2').hide();
+				$('.icon-sun-3').show();				
+				$('.icon-sun-3').on('click', function() {
+					quickSet();
+				});
+			});			
+		}
 	}
 	var wait = setTimeout(function() {
 		sunset();
